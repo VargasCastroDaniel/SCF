6bcb7c5d8cc86a724fd1a6755a25f9b705cb8746
Daniel Vargas Castro <104041838+VargasCastroDaniel@users.noreply.github.com>
2023-04-17 16:23:23 -0600

    Add files via upload

---
+++ Nueva carpeta/Consulta2.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consulta_Alto_Grado_2</title>
+    <link rel="stylesheet" href="./estilo_button.css">
+</head>
+<body>
+    <?php
+        $FechaI = $_GET['FechaI'];
+        $FechaF = $_GET['FechaF'];
+
+        $serverName = "DESKTOP-34JAGCF";
+        $connectionOptions = array(
+            "Database" => "SistemaControlFinca",
+            "UID" => "Daniel",
+            "PWD" => "proyecto"
+        );
+        
+        $conn = sqlsrv_connect($serverName, $connectionOptions) or die('Error al conectar con la base de datos');
+        $tsql= "execute SCF.Consulta2 '$FechaI', '$FechaF'";
+        $getResults= sqlsrv_query($conn, $tsql);
+        
+        $tsq= "select * from SCF.Prints";
+        $getResult= sqlsrv_query($conn, $tsq);
+        while ($row = sqlsrv_fetch_array($getResult, SQLSRV_FETCH_ASSOC)) 
+        {
+            echo ("$row[Dato]<hr>");
+        }
+
+    ?>
+    <div class="Regresar">
+            <input type="button" value="Regresar al inicio" onclick="window.location='vis_general.html'">
+    </div>
+    
+    <div class="Atras">
+            <input type="button" value="Atras" onclick="window.location='Consulta2.html'">
+    </div>
+</body>
+</html>
